082b8935ee7095000c952d2895e8b9ddaec81880
scottdiddams <sdiddams@gmail.com>
2021-01-26 08:35:37 -0500

    update

---
+++ quizzes/C3Quiz02/C3Quiz02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace C3Quiz02
+{
+    class Program
+    {
+        static void Main(string[] lenny)
+        {
+            Console.WriteLine("C Sharp Quiz 2");
+            Console.WriteLine("Calculating phi using a recursive method by the Fibonacci series");
+            Console.WriteLine($"Calling, CalcPhi");
+            CalcPhi(1, 1, 1, 40);
+            Console.ReadLine();
+        }
+
+        public static void CalcPhi(double a, double b, double counter, double limit)
+        {
+            RecursiveFun(a, b, counter, limit);
+           
+        }
+
+        public static void RecursiveFun(double a, double b, double counter, double limit)
+
+        {
+            if (counter <= limit)
+            {
+                double C = (b / a);
+                Console.WriteLine("{0} & {1} = {2}, ", a, b, C);
+                RecursiveFun(b, a + b, counter + 1, limit);
+            }
+
+        }
+    }
+}
