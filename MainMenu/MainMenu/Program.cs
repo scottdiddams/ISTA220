449e1aa61c9e97bb11a859becdae8597d2b89587
scottdiddams <sdiddams@gmail.com>
2021-02-08 08:20:53 -0500

    update

---
+++ MainMenu/MainMenu/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace MainMenu
+{
+    class Program
+    {
+        static void Main(string[] lenny)
+        {
+            bool startMenu = true;
+            while (startMenu)
+            {
+                Console.Clear();
+                startMenu = MainMenu();
+            }
+        }
+
+        private static bool MainMenu()
+        {
+            Console.WriteLine("What would you like to do");
+            Console.WriteLine("1: ");
+            Console.WriteLine("2: ");
+            Console.WriteLine("3: ");
+            Console.WriteLine("4: exit");
+            int menu = int.Parse(Console.ReadLine());
+
+            switch (menu)
+            {
+                case 1:
+                    methodChoice1();
+                    return true;
+
+                case 2:
+                    MethodChoice2();
+                    return true;
+
+                case 3:
+                    MethodChoice3();
+                    return true;
+
+                case 4:
+                    Console.WriteLine("GoodBye");
+                    Console.ReadLine();
+                    return false;
+
+                default:
+                    Console.WriteLine("Thats not an option");
+                    Console.ReadLine();
+                    return true;
+            }
+
+
+        }
+
+
+
+        private static void methodChoice1()
+        {
+            Console.WriteLine("Choice 1");
+            Console.ReadLine();
+        }
+
+
+        private static void MethodChoice3()
+        {
+            Console.WriteLine("Choice 3");
+            Console.ReadLine();
+        }
+
+        private static void MethodChoice2()
+        {
+            Console.WriteLine("Choice 2");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ quizzes/C3Quiz04/C3Quiz04/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace C3Quiz04
+{
+        class Firearm
+          
+        {
+        public string Name;
+        public string Action;
+            internal void behave(string name, string action)
+        {
+            Name = name;
+            Action = action;
+            Console.WriteLine($"I am a {name}, and I go {action}");
+        }
+        }
+        class Program
+        {
+
+            static void Main(string[] lenny)
+        {
+            Console.WriteLine("Welcome to C# Quiz 04");
+            
+            Firearm shotgun = new Firearm();
+            shotgun.Name = "shotgun";
+            shotgun.Action = "Boom";
+            shotgun.behave(shotgun.Name, shotgun.Action);
+
+            Firearm rifle = new Firearm();
+            rifle.Name = "Rifle";
+            rifle.Action = "Bang";
+            rifle.behave(rifle.Name, rifle.Action);
+
+            Firearm pistol = new Firearm();
+            pistol.Name = "pistol";
+            pistol.Action = "Pop";
+            pistol.behave(pistol.Name, pistol.Action);
+
+            Console.ReadLine();
+        }
+    }
+}
